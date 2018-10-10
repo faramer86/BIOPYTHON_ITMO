2926d73b8a8a4ebea24463f185d8da7e4adf629a
faramer86 <36647839+faramer86@users.noreply.github.com>
2018-10-11 00:13:52 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,8 +1,33 @@
+#  1
+#  Implement the following generators in file task3/task3.py
+
+import itertools
+from functools import reduce
+
+
+def squares(a):
+    for i in a:
+        yield i*i
+
+
+def repeatntimes(elems, n):
+    for i in range(n):
+        yield from elems
+
+
+def evens(x):
+    if x % 2 == 0:
+        yield from itertools.count(x, 2)
+    else:
+        yield from itertools.count(x+1, 2)
+
+
+def digitsumdiv(p):
+    yield from itertools.count(p, p)
+
 #   2
 #   Implement the following functions in file task3/task3.py
 #   using map, filter, zip, reduce:
-
-from functools import reduce
 
 
 def extractnumbers(s):
